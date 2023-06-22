b7ca5ab2cd8801141a91c47a48411107b0cf1b2f
rianmz-genz <adrianji1945@gmail.com>
2023-06-22 19:48:40 +0700

    add validation

--- resources/js/Pages/EditNews.jsx
+++ resources/js/Pages/EditNews.jsx
@@ -8,8 +8,9 @@ export default function EditNews(props) {
     const [category, setCategory] = useState("");
 
     const data = { id: props.myNews.id, title, description, category };
-    console.log(data)
+    console.log(data);
     const handleSubmit = (e) => {
+        if (title == "" || description == "" || category == "") return;
         e.preventDefault();
         Inertia.post("/news/update", data);
         setTitle("");
@@ -21,44 +22,44 @@ export default function EditNews(props) {
             <Head title={props.title} />
             <Navbar user={props.auth.user} />
             <form
-                    onSubmit={handleSubmit}
-                    className="max-w-7xl mx-auto sm:px-6 lg:px-8"
-                >
-                    <h1 className="text-black font-bold ml-8">Edit News</h1>
-                    <div className="p-6 bg-white border-b border-gray-200 text-black">
-                        <input
+                onSubmit={handleSubmit}
+                className="max-w-7xl mx-auto sm:px-6 lg:px-8"
+            >
+                <h1 className="text-black font-bold ml-8">Edit News</h1>
+                <div className="p-6 bg-white border-b border-gray-300 text-black">
+                    <input
                         required
-                            type="text"
-                            placeholder="Judul"
-                            className="m-2 input input-bordered w-full bg-white"
-                            onChange={(title) => setTitle(title.target.value)}
-                            defaultValue={props.myNews.title}
-                        />
-                        <input
+                        type="text"
+                        placeholder="Judul"
+                        className="m-2 input input-bordered w-full bg-white"
+                        onChange={(title) => setTitle(title.target.value)}
+                        defaultValue={props.myNews.title}
+                    />
+                    <input
                         required
-                            type="text"
-                            placeholder="Deskripsi"
-                            className="m-2 input input-bordered w-full bg-white"
-                            onChange={(description) =>
-                                setDescription(description.target.value)
-                            }
-                            defaultValue={props.myNews.description}
-                        />
-                        <input
+                        type="text"
+                        placeholder="Deskripsi"
+                        className="m-2 input input-bordered w-full bg-white"
+                        onChange={(description) =>
+                            setDescription(description.target.value)
+                        }
+                        defaultValue={props.myNews.description}
+                    />
+                    <input
                         required
-                            type="text"
-                            placeholder="Kategori"
-                            className="m-2 input input-bordered w-full bg-white"
-                            onChange={(category) =>
-                                setCategory(category.target.value)
-                            }
-                            defaultValue={props.myNews.category}
-                        />
-                        <button className="btn btn-primary m-2" type="submit">
-                            Submit
-                        </button>
-                    </div>
-                </form>
+                        type="text"
+                        placeholder="Kategori"
+                        className="m-2 input input-bordered w-full bg-white"
+                        onChange={(category) =>
+                            setCategory(category.target.value)
+                        }
+                        defaultValue={props.myNews.category}
+                    />
+                    <button className="btn btn-primary m-2" type="submit">
+                        Submit
+                    </button>
+                </div>
+            </form>
         </div>
     );
 }
