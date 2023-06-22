22f2de41804f91f52f62b2ced4e9264ddcd9bfb2
rianmz-genz <adrianji1945@gmail.com>
2023-06-22 19:58:30 +0700

    add member

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -14,12 +14,42 @@ export default function Homepage(props) {
                     <NewsList news={props.news.data} />
                 </div>
                 <aside className="w-4/12 min-h-screen p-8 border-l border-gray-300/40 sticky top-6">
-                    <h1 className="text-2xl font-semibold">
+                    <h1 className="text-2xl text-center font-semibold">
                         Siapa yang membuat?
                     </h1>
-                    <ul>
-                        <li>Adrian Aji Septa</li>
+                    <ul className="mt-4 space-y-3">
+                        <li className="px-4 py-2 bg-gray-300/30">
+                            <h1 className="text-lg font-semibold">
+                                Adrian Aji Septa
+                            </h1>
+                            <p>22SA11A080</p>
+                        </li>
+                        <li className="px-4 py-2 bg-gray-300/30">
+                            <h1 className="text-lg font-semibold">
+                                Amar Al Farizi
+                            </h1>
+                            <p>22SA11A080</p>
+                        </li>
+                        <li className="px-4 py-2 bg-gray-300/30">
+                            <h1 className="text-lg font-semibold">
+                                Mutia Dwi Anggraeni
+                            </h1>
+                            <p>22SA11A072</p>
+                        </li>
+                        <li className="px-4 py-2 bg-gray-300/30">
+                            <h1 className="text-lg font-semibold">
+                                Anggie Maesyaroh
+                            </h1>
+                            <p>22SA11A086</p>
+                        </li>
+                        <li className="px-4 py-2 bg-gray-300/30">
+                            <h1 className="text-lg font-semibold">
+                                Siti Nahla Amalina
+                            </h1>
+                            <p>22SA21A166</p>
+                        </li>
                     </ul>
+                    <p className="mt-2">terimakasih buat lo semua n...</p>
                 </aside>
             </div>
             <div className="flex justify-center items-center">
