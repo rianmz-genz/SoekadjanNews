1b5aeb804772803ba6cb41d9a2c7b288a5ee85a7
rianmz-genz <adrianji1945@gmail.com>
2023-06-22 19:48:13 +0700

    remove search

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,22 +1,14 @@
 import { Link } from "@inertiajs/inertia-react";
 
-
 const Navbar = ({ user }) => {
     return (
-        <div className="navbar bg-white border-b border-slate-900 mb-3">
+        <div className="navbar bg-white border-b border-gray-300/40 mb-3">
             <div className="flex-1">
                 <a className="btn btn-ghost normal-case text-xl">
                     SoekadjanNews
                 </a>
             </div>
             <div className="flex-none gap-2">
-                <div className="form-control">
-                    <input
-                        type="text"
-                        placeholder="Search"
-                        className="input input-bordered bg-white"
-                    />
-                </div>
                 <div className="dropdown dropdown-end">
                     <label
                         tabIndex="0"
@@ -30,31 +22,43 @@ const Navbar = ({ user }) => {
                         tabIndex="0"
                         className="mt-3 p-2 shadow menu menu-compact dropdown-content text-black rounded-box w-52 bg-white"
                     >
-                        {!user ? 
+                        {!user ? (
                             <>
                                 <li>
-                                    <Link href={route('login')} as="button">Login</Link>
+                                    <Link href={route("login")} as="button">
+                                        Login
+                                    </Link>
                                 </li>
                                 <li>
-                                    <Link href={route('register')} as="button">Register</Link>
+                                    <Link href={route("register")} as="button">
+                                        Register
+                                    </Link>
                                 </li>
                             </>
-                         : 
+                        ) : (
                             <>
                                 <li>
-                                    <Link href={route('dashboard')} as="button" className="justify-between">
+                                    <Link
+                                        href={route("dashboard")}
+                                        as="button"
+                                        className="justify-between"
+                                    >
                                         Dashboard
                                         <span className="badge">New</span>
                                     </Link>
                                 </li>
+
                                 <li>
-                                  <Link>Setting</Link>
-                                </li>
-                                <li>
-                                  <Link href={route('logout')} as="button" method="post">Logout</Link>
+                                    <Link
+                                        href={route("logout")}
+                                        as="button"
+                                        method="post"
+                                    >
+                                        Logout
+                                    </Link>
                                 </li>
                             </>
-                        }
+                        )}
                     </ul>
                 </div>
             </div>
